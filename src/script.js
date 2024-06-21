fbde3b0a6083e4ba9244c1d9512b133227964c15
guilherme-osanai <guilherme.osanai@engnetconsultoria.com.br>
2024-06-21 19:13:45 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,23 +1,58 @@
 //função de mudar imagem pelo id e pela url
 function changeImage(id, url) {
-  document.getElementById(id).src = url;
-}
-//função de mudar texto pelo id e pelo texto
-function changeText(id, text) {
-  document.getElementById(id).innerText = text;
+    document.getElementById(id).src = url;
+  }
+  //função de mudar texto pelo id e pelo texto
+  function changeText(id, text) {
+    document.getElementById(id).innerText = text;
+  }
+  
+  // Daqui para baixo voce ira escrever
+  // o código para resolver o desafio
+  
+let primeiroPokemon = 12;
+
+
+// Função para buscar dados dos Pokemons na PokeAPI
+async function fetchPokemonData(pokemonName) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Erro ao buscar dados do Pokemon:', error);
+    return null;
+  }
 }
 
-// Daqui para baixo voce ira escrever
-// o código para resolver o desafio
+// Atualiza os dados do Pokemon na página
+async function updatePokemon(pokemonName) {
+  const pokemonData = await fetchPokemonData(pokemonName);
+  if (pokemonData) {
+    changeText('name', pokemonData.name);
+    changeImage('img_sprite_front_default', pokemonData.sprites.front_default);
+  }
+}
 
+updatePokemon(primeiroPokemon);
+
+// Carrega o Pokemon anterior
 function previousPokemon() {
-  alert("Pokemon Anterior");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Anterior");
+  if (primeiroPokemon > 12){
+    primeiroPokemon -= 1;
+  updatePokemon(primeiroPokemon)
+  } else {
+    primeiroPokemon = 120;
+    updatePokemon(primeiroPokemon)
+  }
 }
-
+// Carrega o Pokemon seguinte
 function nextPokemon() {
-  alert("Pokemon Seguinte");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Seguinte");
+  if (primeiroPokemon < 120){
+  primeiroPokemon += 1;
+  updatePokemon(primeiroPokemon);
+  } else {
+    primeiroPokemon = 12;
+    updatePokemon(primeiroPokemon)
+  }
 }
